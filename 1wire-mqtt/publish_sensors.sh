7a43035245c025924689dfc079be290336b2ded7
baschno <bastian.schnorbus@gmail.com>
2018-07-26 19:56:37 +0000

    new script reading all ds sensor and publish it

--- 1wire-mqtt/publish_sensors.sh
+++ 1wire-mqtt/publish_sensors.sh
@@ -3,13 +3,14 @@
 usage() {
   cat <<EndOfMessage
   NAME:
-    $(basename $0) - Become space developer
+    $(basename $0) - Read onewire ds sensor and publish to mqtt
   USAGE:
      bin/$(basename $0) [<options>]
   OPTIONS:
     -h|--help            Print this help
     --mq-user            Mosquitto User
     --mq-pwd             Moquitto Pwd
+    --mq-host
 
 EndOfMessage
 exit
@@ -20,14 +21,19 @@ while [[ $# -gt 0 ]] ;do
     -h|--help)
       usage
     ;;
-    --cf-admin-pwd)
+    --mq-user)
       [ -z "$2" ] && die "ERROR: ${1} requires a non-empty option argument."
-      CONTROLLER_PASSWORD="$2"
+      MQ_USER="$2"
       shift
     ;;
-    --cf-user)
+    --mq-pwd)
       [ -z "$2" ] && die "ERROR: ${1} requires a non-empty option argument."
-      CF_USER="$2"
+      MQ_PWD="$2"
+      shift
+    ;;
+    --mq-host)
+      [ -z "$2" ] && die "ERROR: ${1} requires a non-empty option argument."
+      MQ_HOST="$2"
       shift
     ;;
     --)              # End of all options.
@@ -43,19 +49,26 @@ while [[ $# -gt 0 ]] ;do
   shift
 done
 
-if [[ -z $CONTROLLER_PASSWORD ]]; then
+if [[ -z $MQ_HOST ]]; then
   usage
 fi
-if [[ -z $CF_USER ]]; then
+if [[ -z $MQ_USER ]]; then
+  usage
+fi
+if [[ -z $MQ_PWD ]]; then
   usage
 fi
 
-for sensor_path in /sys/bus/w1/devices/28-* ; do 
+for sensor_path in /sys/bus/w1/devices/28-* ; do
   echo $sensor_path
   sensor_name=$(echo $sensor_path | cut -d'/' -f6)
-  echo $sensor_name
-  sensor_val=$(cat /sys/bus/w1/devices/28-00000595f59e/w1_slave | grep 't=' | cut -d'=' -f2)
+  echo "K: $sensor_name"
+  sensor_val=$(cat "$sensor_path/w1_slave" | grep 't=' | cut -d'=' -f2)
   sensor_temp=$(echo $sensor_val | awk '{print $1 / 1000}')
-  echo $sensor_temp
-done
+  echo "V: $sensor_temp"
+  mqtt_topic="sensors/onewire/$sensor_name"
+  ts=$(date +"%Y-%m-%d_%H-%M")
 
+  eval "mosquitto_pub -h $MQ_HOST -u $MQ_USER -P $MQ_PWD -i raspi-1b-1 -t $mqtt_topic -m '{\"temperature\":$sensor_temp, \"timestamp\":\"$ts\"}'"
+
+done
